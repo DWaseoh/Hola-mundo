3f01fd457fe899977a687f044fd8183b05264937
Daniel <dwaseoh@protonmail.com>
2020-03-30 20:18:37 +0200

    Primera versión de código

---
+++ Hola mundo.py
@@ -0,0 +1,9 @@
+n=input("¿Cómo se llama tu personaje?")
+
+c=input("Qué clase tiene "+n+"? Guerrero, mago o pícaro")
+
+a=input("Qué armas maneja "+n+"?")
+
+l=input("Dónde nació "+n+"?")
+
+print("Nuestro "+c+" "+n+" nació en "+l+" y desde pequeño se adiestro con "+a)
