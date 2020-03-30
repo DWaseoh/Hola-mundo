93ca5f2313f98aeb6304e5306069d4a941a530c7
Daniel <dwaseoh@protonmail.com>
2020-03-30 21:15:06 +0200

    Le estoy añadiendo if

--- Hola mundo.py
+++ Hola mundo.py
@@ -1,6 +1,17 @@
 n=input("¿Cómo se llama tu personaje?")
 
+'''aqui invoco la variable c2'''
 c2=input("Qué clase tiene "+n+"? Guerrero, mago o pícaro")
+
+if c2 == "sacerdote":
+    print("Bendigame por favor")
+elif c2 == "picaro":
+    print("Te gusta robar eh?")
+elif c2 == "guerrero" or  n == "dani":
+        print("Te gusta ir al gym, verdad?")
+        print("MENTIROSO")
+else:
+    print("No te he dicho eso")
 
 a=input("Qué armas maneja "+n+"?")
 
