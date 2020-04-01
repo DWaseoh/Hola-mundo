df46ae652d56f9c1b625229168480417ea770436
Daniel <dwaseoh@protonmail.com>
2020-04-01 19:52:02 +0200

    Funciones

--- Hola mundo.py
+++ Hola mundo.py
@@ -1,22 +1,44 @@
+'''Nombre del personaje'''
+
+def comprueba(primero, segundo):
+    if primero.upper() == segundo.upper():
+        return True
+    else:
+        return False
+
 n = input("¿Cómo se llama tu personaje?")
 
-'''aqui invoco la variable c2'''
-c2 = input("Qué clase tiene " + n + "? Guerrero, mago o pícaro")
-
-if c2 == "sacerdote":
-    print("Bendigame por favor")
-elif c2 == "picaro":
-    print("Te gusta robar eh?")
-elif c2 == "guerrero" or n == "dani":
-        print("Te gusta ir al gym, verdad?")
-        print("MENTIROSO")
+if comprueba("javi", n):
+    n = "capullo"
+    print("Ahh... con que eres un capullo.")
+    c = input("¿Qué clase tienes, " + n + "? Guerrero, mago o pícaro")
 else:
-    print("No te he dicho eso")
+    print("Me gusta, ¿Es extranjero?")
+    c = input("¿Qué clase tiene " + n + "? Guerrero, mago o picaro")
 
-a = input("Qué armas maneja "+n+"?")
+'''Clase del personaje y armas'''
+if comprueba("MAGO", c):
+    print("Eres una de esas ratas de biblioteca, ¿Verdad?")
+    a = "el baculo"
+    print("Si es así las armas que manejas son los baculos.")
+elif c == "picaro" or c == "Picaro":
+    print("Tranquilo, estaré pendiente de mi cartera a partir de ahora")
+    a = "las dagas"
+    print("Si es así las armas que manejas son las dagas.")
+elif c == "guerrero" or c == "Guerrero":
+    print("Uff, pero que musculos chaval.")
+    print("Si es así puedes manejar el arma que quieras")
+    a = input("¿Qué arma quieres manejar?")
+else:
+    print("¿Cómo que " + c + "? ¿Eso que mierda es? Eres un graciosillo por lo que veo. Pues tu clase será «retrasado»")
+    c = "retrasado"
+    print("Como eres un retrasado no puedes manejar ningún arma ¯\_( ㆆ u ㆆ )_/¯")
+    print("Así que no puedes jugar, lo siento .|.( ㆆ u ㆆ )")
 
-l = input("Dónde nació "+n+"?")
-
-print ("Nuestro "+c2+" "+n+" nació en "+l+" y desde pequeño se adiestro con "+a)
-
-print ("Entonces "+n+" murió estrepitosamente")
+if c == "mago" or c == "Mago" or c == "picaro" or c == "Picaro" or c == "guerrero" or c == "Guerrero":
+    l = input("¿Dónde naciste, " + n + "?")
+    if l == "Avila" or l == "avila":
+        print("Vaya mierdas estas hecho")
+    elif l == "Sevilla" or l == "sevilla":
+        print("Vaya puto amo")
+    print ("Nuestro " + c + " " + n + " nació en " + l + " y desde pequeño se adiestro con " + a)
---
+++ Strings.py
@@ -0,0 +1,15 @@
+#x = input("Dime tu nombre: ")
+
+def suma(a, b):
+    if a < b:
+        return "ERROR"
+    a = a + 1
+    b = 0
+    r = a+b
+    return r
+
+
+#print(x.upper() + " kajsg dkjasgdakjsdg asjgjkasd\njkgasdjhasgdjhasd")
+
+print(str(suma(3, 2)))
+
